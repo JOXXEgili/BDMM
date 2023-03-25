831b10db99a4c712a3ca648e5969800e6a00d3b9
Angel Cerecero <91641441+KETTZO@users.noreply.github.com>
2023-03-25 16:19:46 -0600

    conexion bd

---
+++ conexion.php
@@ -0,0 +1,31 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "capaIntermediaMiercoles1";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+else
+{
+  echo "Connected successfully";
+}
+
+
+$sql = "INSERT INTO tabla1 (firstname, lastname, email)
+VALUES ('Juan', 'Perez', 'juanito@perez.com')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+
+
+$conn->close();
+?>
